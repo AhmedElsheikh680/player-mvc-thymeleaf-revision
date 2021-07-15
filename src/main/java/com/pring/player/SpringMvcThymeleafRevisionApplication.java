67d9a27d59522e6508fe8ee70c5a79fc5020e94e
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-07-15 11:54:58 +0200

    init project

---
+++ src/main/java/com/pring/player/SpringMvcThymeleafRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.pring.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymeleafRevisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcThymeleafRevisionApplication.class, args);
+    }
+
+}
